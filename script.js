7ce3faa9114b9c73c49502c6e91ef565c29fa164
Bmorales-cc <mzbrayan12@gmail.com>
2025-02-27 09:06:30 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,14 @@
 document.getElementById('titulo-tienda').addEventListener('click', function() {
     document.getElementById('productos').scrollIntoView({ behavior: 'smooth' });
 });
+const contenedorProductos = document.querySelector('.contenedor-productos');
+
+// Pausar la animación al pasar el mouse
+contenedorProductos.addEventListener('mouseenter', () => {
+    contenedorProductos.style.animationPlayState = 'paused';
+});
+
+// Reanudar la animación al quitar el mouse
+contenedorProductos.addEventListener('mouseleave', () => {
+    contenedorProductos.style.animationPlayState = 'running';
+});
